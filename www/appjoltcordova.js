48e6f9fee5d4f2ab60795bcc991ec18bbd30a24f
Hagai Hanan <quadude@gmail.com>
2015-08-19 17:56:01 +0300

    Fixed typo

--- www/appjoltcordova.js
+++ www/appjoltcordova.js
@@ -5,43 +5,43 @@ function AppjoltCordova() {
 
 
 AppjoltCordova.prototype.updateUserSegment = function(userSegment, successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "updateUserSegment", "AppjoltPlugin", "updateUserSegment", [userSegment]); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "updateUserSegment"), "AppjoltPlugin", "updateUserSegment", [userSegment]); 
 };
 
 AppjoltCordova.prototype.addUserSegment = function(userSegment, successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "addUserSegment", "AppjoltPlugin", "addUserSegment", [userSegment]); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "addUserSegment"), "AppjoltPlugin", "addUserSegment", [userSegment]); 
 };
 
 AppjoltCordova.prototype.removeUserSegment = function(userSegment, successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "removeUserSegment", "AppjoltPlugin", "removeUserSegment", [userSegment]); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "removeUserSegment"), "AppjoltPlugin", "removeUserSegment", [userSegment]); 
 };
 
 AppjoltCordova.prototype.clearUserSegment = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "clearUserSegment", "AppjoltPlugin", "clearUserSegment", []); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "clearUserSegment"), "AppjoltPlugin", "clearUserSegment", []); 
 };
 
 AppjoltCordova.prototype.identify = function(identify, successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "identify", "AppjoltPlugin", "identify", [identify]); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "identify"), "AppjoltPlugin", "identify", [identify]); 
 };
 
 AppjoltCordova.prototype.showEULA = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "showEULA", "AppjoltPlugin", "showEULA", []); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "showEULA"), "AppjoltPlugin", "showEULA", []); 
 };
 
 AppjoltCordova.prototype.useCustomEULA = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "useCustomEULA", "AppjoltPlugin", "useCustomEULA", []); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "useCustomEULA"), "AppjoltPlugin", "useCustomEULA", []); 
 };
 
 AppjoltCordova.prototype.userAcceptedCustomEULA = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "userAcceptedCustomEULA", "AppjoltPlugin", "userAcceptedCustomEULA", []); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "userAcceptedCustomEULA"), "AppjoltPlugin", "userAcceptedCustomEULA", []); 
 };
 
 AppjoltCordova.prototype.isGooglePlayInstall = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "isGooglePlayInstall", "AppjoltPlugin", "isGooglePlayInstall", []); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "isGooglePlayInstall"), "AppjoltPlugin", "isGooglePlayInstall", []); 
 };
 
 AppjoltCordova.prototype.setIncognito = function(incognito, successCallback, errorCallback) {
-	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "setIncognito", "AppjoltPlugin", "setIncognito", [incognito]); 
+	cordova.exec(successCallback, this._getErrorCallback(errorCallback, "setIncognito"), "AppjoltPlugin", "setIncognito", [incognito]); 
 };
 
 
